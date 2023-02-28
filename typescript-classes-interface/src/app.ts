a01c46294122b03d654f8b1a6db98b207a76b195
Andhika Prasetya <andhikapr081@gmail.com>
2023-02-28 15:43:59 +0700

    typescript-classes-interface introduction to class and it's property / instance learning how to use this.keyword

--- typescript-classes-interface/src/app.ts
+++ typescript-classes-interface/src/app.ts
@@ -1,1 +1,16 @@
-// Code goes here!
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  // this: Deparment -> to make sure this keyword always refer to instance inside the Department class
+  describe(this: Department) {
+    console.log(`Deparment: ${this.name}`);
+  }
+}
+
+const account = new Department('andhika'); // initiate class
+
+account.describe(); // call the describe() method property

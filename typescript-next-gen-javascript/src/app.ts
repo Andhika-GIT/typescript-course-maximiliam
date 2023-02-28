759b7e325f3cad344c6ab64d704b830a9a240922
Andhika Prasetya <andhikapr081@gmail.com>
2023-02-28 14:43:08 +0700

    typescript-next-gen-javascript working with destructuring and spread operator

--- typescript-next-gen-javascript/src/app.ts
+++ typescript-next-gen-javascript/src/app.ts
@@ -1,1 +1,36 @@
-// Code goes here!
+// type AddFunctionType = (a: number, b: number) => number;
+// type PrintFunctionType = (a: string | number) => void;
+
+// const add: AddFunctionType = (a, b) => a + b;
+
+// const printOutput: PrintFunctionType = (output) => console.log(output);
+
+// ----------**------------//
+
+const passiveHobbies = ['sports', 'cooking'];
+const activeHobbies = ['hiking'];
+
+const person: {
+  firstName: string;
+  age: number;
+} = {
+  firstName: 'andhika',
+  age: 22,
+};
+
+const hobbies = [...passiveHobbies, ...activeHobbies];
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+const { firstName, age } = person;
+
+// ----------**------------//
+
+type AddFunctionType = (...numbers: number[]) => void;
+
+const add: AddFunctionType = (...numbers) => {
+  let total: number = numbers.reduce((curResult, curValue) => curResult + curValue);
+  console.log(total);
+};
+
+add(5, 10, 12, 20, 7);

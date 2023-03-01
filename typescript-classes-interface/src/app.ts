d54042a3d5fb022907efff45c9dccff42516ea3d
Andhika Prasetya <andhikapr081@gmail.com>
2023-03-01 15:56:45 +0700

    typescript-classes-interface inheritance setter and getter

--- typescript-classes-interface/src/app.ts
+++ typescript-classes-interface/src/app.ts
@@ -1,12 +1,7 @@
 class Department {
-  // private id: string
-  // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {
-    // this.name = n;
-    // this.id = id
-  }
+  constructor(private readonly id: string, public name: string) {}
 
   // this: Deparment -> to make sure this keyword always refer to instance inside the Department class
   describe(this: Department) {
@@ -18,15 +13,73 @@ class Department {
   }
 
   printEmployeeInformation() {
-    console.log(this.employees.length);
+    // console.log(this.employees.length);
     console.log(this.employees);
   }
 }
 
-const account = new Department('123123123', 'andhika'); // initiate class
+// child classes //
+class ITDepartment extends Department {
+  constructor(id: string, name: string, public admins: string[]) {
+    super(id, name); // pass the id to the Department class
+  }
 
-account.describe(); // call the describe() method property
+  printAdmin(this: ITDepartment) {
+    console.log(`it deparment leader name : ${this.name}, crew name : ${this.admins}  `);
+  }
 
-account.addEmployee('hendry');
-account.addEmployee('aldi');
-account.printEmployeeInformation();
+  addEmployee(name: string): void {
+    this.employees.push(name);
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, name: string, public admins: string[], private reports: string[]) {
+    super(id, name); // pass the id to the Department class
+  }
+
+  // accessing private property using getter function
+  get getReports() {
+    if (this.reports.length > 0) return this.reports;
+
+    return 'No report found';
+  }
+
+  // modify private property using setter function
+  set addReports(value: string) {
+    if (!value) return;
+
+    this.reports.push(value);
+  }
+
+  printAdmin(this: ITDepartment) {
+    console.log(`accounting deparment leader name : ${this.name}, crew name : ${this.admins}  `);
+  }
+
+  addEmployee(name: string): void {
+    this.employees.push(name);
+  }
+}
+
+// testing //
+
+const deparment = new Department('123123123', 'andhika'); // initiate class
+
+deparment.describe(); // call the describe() method property
+
+const it = new ITDepartment('IT', 'Furfio', ['herman', 'joko']);
+
+it.printAdmin();
+
+///-----////
+
+const accounting = new AccountingDepartment('Accounting', 'Julia', ['Rio'], []);
+
+accounting.printAdmin();
+
+accounting.addEmployee('julio');
+accounting.printEmployeeInformation();
+accounting.addReports = 'report sunday';
+
+const reportsData = accounting.getReports;
+console.log(reportsData);

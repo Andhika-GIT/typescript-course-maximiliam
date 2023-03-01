1a3aed5e18576a64d26ee5508660dd5443c631d4
Andhika Prasetya <andhikapr081@gmail.com>
2023-03-01 13:04:59 +0700

    typescript-classes-interface using private, public and readonly modifier

--- typescript-classes-interface/src/app.ts
+++ typescript-classes-interface/src/app.ts
@@ -1,16 +1,32 @@
 class Department {
-  name: string;
+  // private id: string
+  // private name: string;
+  private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, public name: string) {
+    // this.name = n;
+    // this.id = id
   }
 
   // this: Deparment -> to make sure this keyword always refer to instance inside the Department class
   describe(this: Department) {
-    console.log(`Deparment: ${this.name}`);
+    console.log(`Deparment Name : ${this.name}, id: ${this.id}`);
+  }
+
+  addEmployee(this: Department, employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const account = new Department('andhika'); // initiate class
+const account = new Department('123123123', 'andhika'); // initiate class
 
 account.describe(); // call the describe() method property
+
+account.addEmployee('hendry');
+account.addEmployee('aldi');
+account.printEmployeeInformation();

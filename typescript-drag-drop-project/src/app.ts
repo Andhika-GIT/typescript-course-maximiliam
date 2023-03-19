e29e67a2c31c3d8c74f2dcd1509b59deeea0d0b1
Andhika Prasetya <andhikapr081@gmail.com>
2023-03-19 13:19:58 +0700

    typescript-drag-drop-project add simple validation

--- typescript-drag-drop-project/src/app.ts
+++ typescript-drag-drop-project/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement;
   hostElement;
   element;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = <HTMLTemplateElement>document.getElementById('project-input');
@@ -12,7 +15,46 @@ class ProjectInput {
 
     // take the first element from the copy template content which is the <form> element
     this.element = <HTMLFormElement>importedNode.firstElementChild;
+    this.element.setAttribute('id', 'user-input');
+
+    // select all the input field in the form based on the id
+    this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title');
+    this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description');
+    this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people');
+
+    this.submitHandler();
+
+    // run the attach function
     this.attach();
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (enteredTitle.trim().length === 0 || enteredPeople.trim().length === 0 || enteredDescription.trim().length === 0) {
+      alert('Invalid input, please try again');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.element.reset();
+  }
+
+  private submitHandler() {
+    this.element.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const userInput = this.gatherUserInput();
+      if (Array.isArray(userInput)) {
+        const [title, desc, people] = userInput;
+        console.log(title, desc, people);
+        this.clearInputs();
+      }
+    });
   }
 
   private attach() {

f5aeaefffe14de3127087c25cac03b91347c4218
Andhika Prasetya <andhikapr081@gmail.com>
2023-04-05 14:26:42 +0700

    typescript-share-place-project we stopped the project here the google geocoding requires credit card "sigh" maximiliam course kinda sucks

--- typescript-share-place-project/src/app.ts
+++ typescript-share-place-project/src/app.ts
@@ -1,1 +1,12 @@
 // Code goes here!
+const form = <HTMLFormElement>document.querySelector('form');
+const addressInput = <HTMLInputElement>document.getElementById('address');
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  //   const enteredAdress = addressInput.value;
+
+  // send to google API
+}
+
+form.addEventListener('submit', searchAddressHandler);
